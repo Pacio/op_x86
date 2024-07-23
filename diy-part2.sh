6604fac7dd596e86d1753eddf3dde3de5af667fc
Pacio <4995125@163.com>
2024-07-24 06:09:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,7 @@
 
 # Modify default IP
 sed -i 's/eth1/eth3/g' package/base-files/files/etc/board.d/99-default_network
-sed -i 's/192.168.1.1/10.0.0.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
 mkdir $WORKINGDIR -p
 rm $WORKINGDIR/* -fr
